195940d0d0b79675a43b883124eec5bf33ea8345
June Biadog <jb.biadog@gmail.com>
2022-08-15 17:44:14 -0400

    commit tree 7d7fd3b5c0769e07fa6c5d20330cfae0a680a2c3

---
+++ index.js
@@ -0,0 +1,6 @@
+function logTwoValues(value1, value2) {
+    console.log(`The two values are ${value1} and ${value2}.`);
+  }
+  function introductionWithLanguage(Aki, Ember) {
+    return ("Hi, my name is [name] and I am learning to program in [language")
+  }
